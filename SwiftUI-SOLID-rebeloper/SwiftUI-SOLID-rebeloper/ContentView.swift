ead96ebff36f22c84418a16c9264e92fba4d1c66
[UPFT]KazunoriAoki <kazunori.aoki@up-frontier.jp>
2022-01-05 08:44:08 +0900

    initial commit

---
+++ SwiftUI-SOLID-rebeloper/SwiftUI-SOLID-rebeloper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-SOLID-rebeloper
+//
+//  Created by kazunori.aoki on 2022/01/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-SOLID-rebeloper/SwiftUI-SOLID-rebeloper/SwiftUI_SOLID_rebeloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SOLID_rebeloperApp.swift
+//  SwiftUI-SOLID-rebeloper
+//
+//  Created by kazunori.aoki on 2022/01/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SOLID_rebeloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
