36b4135321fe10280bcf49ef46fb2e76cc9b1269
[UPFT]KazunoriAoki <kazunori.aoki@up-frontier.jp>
2022-01-05 08:53:55 +0900

    add Single Responsibility Principle

--- SwiftUI-SOLID-rebeloper/SwiftUI-SOLID-rebeloper/1_SingleResponsibilityPrinciple/SRP_After.swift
+++ SwiftUI-SOLID-rebeloper/SwiftUI-SOLID-rebeloper/1_SingleResponsibilityPrinciple/SRP_After.swift
@@ -17,3 +17,39 @@ A module or a class should have only one responsibility, and that responsibility
 
 import Foundation
 
+fileprivate class DataHandler {
+
+    let apiHandler: APIHandler
+    let parseHandler: ParseHandler
+    let storageHandler: StorageHandler
+
+    init(apiHandler: APIHandler, parseHandler: ParseHandler, storageHandler: StorageHandler) {
+        self.apiHandler = apiHandler
+        self.parseHandler = parseHandler
+        self.storageHandler = storageHandler
+    }
+
+    func handle() {
+        let data = apiHandler.loadData()
+        let model = parseHandler.parse(data: data)
+        storageHandler.save(model: model)
+    }
+}
+
+fileprivate class APIHandler {
+    func loadData() -> Data {
+        return Data()
+    }
+}
+
+fileprivate class ParseHandler {
+    func parse(data: Data) -> Any {
+        return ""
+    }
+}
+
+fileprivate class StorageHandler {
+    func save(model: Any) {
+        // save
+    }
+}
--- SwiftUI-SOLID-rebeloper/SwiftUI-SOLID-rebeloper/1_SingleResponsibilityPrinciple/SRP_Before.swift
+++ SwiftUI-SOLID-rebeloper/SwiftUI-SOLID-rebeloper/1_SingleResponsibilityPrinciple/SRP_Before.swift
@@ -7,3 +7,25 @@
 
 import Foundation
 
+fileprivate class DataHandler {
+
+    func handle() {
+        let data = loadData()
+
+        let list = parse(data: data)
+
+        save(model: list)
+    }
+
+    func loadData() -> Data {
+        return Data()
+    }
+
+    func parse(data: Data) -> Any {
+        return ""
+    }
+
+    func save(model: Any) {
+        // save
+    }
+}
